dc6c0caf2bc81a146e71c1e482c924f2127ea0cc
esteban zambrano <ezam0000@gmail.com>
2019-03-01 18:22:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-var a= Math.floor(Math.random() * 1000)
-
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
 function add (a,b){
   return (String)
   
